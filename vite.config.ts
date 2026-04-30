47db9f21d1c3190df72df7e0297c5a2394092bde
Foy <s225002731@gmail.com>
2026-04-30 11:15:08 +0800

    fix base path in vite configuration
    - update base property from "sps-newhome" to "/sps-newhome/" in vite.config.ts to ensure proper asset routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base:"sps-newhome",
+  base:"/sps-newhome/",
   plugins: [react(), tailwindcss()],
 })
